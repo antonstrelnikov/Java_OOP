c284e7f27cf6ad1f0c70546b1939ccedcf072bce
antonstrelnikov <anton.strelnikov87@gmail.com>
2021-02-07 22:50:23 +0700

    Создание методов getDifference(), getUnion(), getIntersection() в классе Range. Добавление примеров использования методов в классе RangeUsing.

--- Range/src/com/antonstrelnikov/range/Range.java
+++ Range/src/com/antonstrelnikov/range/Range.java
@@ -5,8 +5,8 @@ public class Range {
     private double to;
 
     public Range(double from, double to) {
-        this.from = from;
-        this.to = to;
+        this.from = Math.min(from, to);
+        this.to = Math.max(from, to);
     }
 
     public double getFrom() {
@@ -31,5 +31,51 @@ public class Range {
 
     public boolean isInside(double number) {
         return number >= from && number <= to;
+    }
+
+    public Range getIntersection(Range range1, Range range2) {
+        double from = Math.max(range1.from, range2.from);
+        double to = Math.min(range1.to, range2.to);
+
+        if (from < to) {
+            return new Range(from, to);
+        }
+
+        return null;
+    }
+
+    public Range[] getUnion(Range range1, Range range2) {
+        if (!range1.isInside(range2.from) && !range2.isInside(range1.from)) {
+            return new Range[]{range1, range2};
+        }
+
+        double from = Math.min(range1.from, range2.from);
+        double to = Math.max(range1.to, range2.to);
+
+        return new Range[] {
+                new Range(from, to)
+        };
+    }
+
+    public Range[] getDifference(Range decreasingRange, Range subtractedRange) {
+        if (!decreasingRange.isInside(subtractedRange.from) && !subtractedRange.isInside(decreasingRange.from)) {
+            return new Range [] {decreasingRange};
+        } else if (subtractedRange.isInside(decreasingRange.from) && subtractedRange.isInside(decreasingRange.to)) {
+            return null;
+        } else if (decreasingRange.isInside(subtractedRange.from) && decreasingRange.isInside(subtractedRange.to)) {
+            return new Range[]{
+                    new Range(decreasingRange.from, subtractedRange.from),
+                    new Range(subtractedRange.to, decreasingRange.to)
+            };
+
+        } else if (decreasingRange.isInside(subtractedRange.from)) {
+            return new Range[] {
+                    new Range(decreasingRange.from, subtractedRange.from)
+            };
+        }
+
+        return new Range[] {
+                new Range(subtractedRange.to, decreasingRange.to)
+        };
     }
 }
---
+++ Range/src/com/antonstrelnikov/range_main/RangeUsing.java
@@ -0,0 +1,199 @@
+package com.antonstrelnikov.range_main;
+
+import com.antonstrelnikov.range.Range;
+
+import java.util.Scanner;
+
+public class RangeUsing {
+    public static final String infoCommand = "info";
+    public static final String setCommand = "set";
+    public static final String checkCommand = "check";
+    public static final String exitCommand = "exit";
+    public static final String unionCommand = "union";
+    public static final String intersectionCommand = "intersection";
+    public static final String differenceCommand = "difference";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите певрое число диапазона: ");
+        double rangeFirstNumber = scanner.nextDouble();
+
+        System.out.println("Введите последнее число диапазона: ");
+        double rangeLastNumber = scanner.nextDouble();
+        scanner.nextLine();
+
+        Range range = new Range(rangeFirstNumber, rangeLastNumber);
+
+        System.out.println("Допустимые команды: ");
+        System.out.println("info - получение информации о диапазоне");
+        System.out.println("check - проверка принадлежности числа диапазону");
+        System.out.println("set - для изменения начального и конечного чисел диапазона");
+        System.out.println("union - объединение двух диапазонов");
+        System.out.println("intersection - пересечение двух диапазонов");
+        System.out.println("difference - разность двух диапазонов");
+        System.out.println("exit - для выхода из программы");
+        System.out.println();
+
+        while (true) {
+            System.out.println("Введите команду: ");
+            String command = scanner.nextLine();
+
+            if (command.equals(exitCommand)) {
+                return;
+            }
+
+            if (command.equals(infoCommand)) {
+                System.out.println("Длина диапазона: " + range.getLength());
+                System.out.println("Первое число диапазона: " + range.getFrom());
+                System.out.println("Последнее число диапазона: " + range.getTo());
+                System.out.println();
+
+                continue;
+            }
+
+            if (command.equals(checkCommand)) {
+                System.out.println("Введите число для проверки: ");
+                double number = scanner.nextDouble();
+                scanner.nextLine();
+
+                if (range.isInside(number)) {
+                    System.out.printf("Число %.2f принадлежит диапазону от %.2f до %.2f%n", number, range.getFrom(), range.getTo());
+                } else {
+                    System.out.printf("Число %.2f не принадлежит диапазону от %.2f до %.2f%n", number, range.getFrom(), range.getTo());
+                }
+
+                System.out.println();
+
+                continue;
+            }
+
+            if (command.equals(setCommand)) {
+                System.out.println("Введите певрое число диапазона: ");
+                rangeFirstNumber = scanner.nextDouble();
+
+                System.out.println("Введите последнее число диапазона: ");
+                rangeLastNumber = scanner.nextDouble();
+                scanner.nextLine();
+
+                range.setFrom(rangeFirstNumber);
+                range.setTo(rangeLastNumber);
+
+                System.out.println("Дианазон задан успешно: ");
+
+                System.out.println();
+
+                continue;
+            }
+
+            if (command.equals(unionCommand)) {
+                System.out.println("Введите певрое число для первого диапазона: ");
+                double from = scanner.nextDouble();
+
+                System.out.println("Введите последнее число для первого диапазона: ");
+                double to = scanner.nextDouble();
+
+                Range range1 = new Range(from, to);
+
+                System.out.println("Введите певрое число для второго диапазона: ");
+                from = scanner.nextDouble();
+
+                System.out.println("Введите последнее число для второго диапазона: ");
+                to = scanner.nextDouble();
+
+                Range range2 = new Range(from, to);
+
+                scanner.nextLine();
+
+                Range[] union = range.getUnion(range1, range2);
+                StringBuilder unionResult = new StringBuilder();
+
+                for (Range unionRange : union) {
+                    unionResult.append(unionRange.getFrom()).append(" - ").append(unionRange.getTo()).append(" ");
+                }
+
+                System.out.println("Результат объединения: " + unionResult);
+
+                System.out.println();
+
+                continue;
+            }
+
+            if (command.equals(intersectionCommand)) {
+                System.out.println("Введите певрое число для первого диапазона: ");
+                double from = scanner.nextDouble();
+
+                System.out.println("Введите последнее число для первого диапазона: ");
+                double to = scanner.nextDouble();
+
+                Range range1 = new Range(from, to);
+
+                System.out.println("Введите певрое число для второго диапазона: ");
+                from = scanner.nextDouble();
+
+                System.out.println("Введите последнее число для второго диапазона: ");
+                to = scanner.nextDouble();
+
+                Range range2 = new Range(from, to);
+
+                scanner.nextLine();
+
+                Range intersection = range.getIntersection(range1, range2);
+
+                if (intersection != null) {
+                    System.out.println("Результат пересечения: " + intersection.getFrom() + " - " + intersection.getTo());
+                } else {
+                    System.out.println("Диапазоны не пересекаются!");
+                }
+
+                System.out.println();
+
+                continue;
+            }
+
+            if (command.equals(differenceCommand)) {
+                System.out.println("Введите певрое число для первого диапазона: ");
+                double from = scanner.nextDouble();
+
+                System.out.println("Введите последнее число для первого диапазона: ");
+                double to = scanner.nextDouble();
+
+                Range range1 = new Range(from, to);
+
+                System.out.println("Введите певрое число для второго диапазона: ");
+                from = scanner.nextDouble();
+
+                System.out.println("Введите последнее число для второго диапазона: ");
+                to = scanner.nextDouble();
+
+                Range range2 = new Range(from, to);
+
+                scanner.nextLine();
+
+                Range[] difference = range.getDifference(range1, range2);
+
+                if (difference == null) {
+                    System.out.println("Результат вычитания - пустое множество");
+
+                    System.out.println();
+
+                    continue;
+                }
+
+                StringBuilder differenceResult = new StringBuilder();
+
+                for (Range unionRange : difference) {
+                    differenceResult.append(unionRange.getFrom()).append(" - ").append(unionRange.getTo()).append(" ");
+                }
+
+                System.out.println("Результат вычитания: " + differenceResult);
+
+                System.out.println();
+
+                continue;
+            }
+
+            System.out.println("Неизвестная команда. Попробуйте еще раз.");
+        }
+    }
+}
